fc74f35cce5832860ee20fbfcfed7874df03823f
Matthew Oleynikov <matthewoleynikov1@gmail.com>
2026-02-06 09:12:46 -0800

    Update lab_1a.py
    Added my intro

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Matthew Oleynikov" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hello! My name is Matthew Oleynikov and I am a junior from Los Angeles. I find my passion in both biology and computer science, and I hope to find an intersection where I can apply ML to the vast biological database. Outside of academics, I play the flute in my school in orchestra, wind ensemble, and marching band, while also being a Boy Scout. I love snowboarding and go to Mammoth mountain annually. I can't wait to collaborate with you guys and hope to find many things in common!")
 
 if __name__ == "__main__":
     main()
